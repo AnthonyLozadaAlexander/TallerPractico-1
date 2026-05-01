58ff09ef2ea94f69c73058f83017df8fce173a98
zDefcon21 <antholozadarequelme@gmail.com>
2026-05-01 17:37:27 -0500

    Update de cambios en el main y Arreglo del codigo para su funcionalidad

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -8,8 +8,8 @@ public class Principal {
 	public static void main(String[] args) {	
 		Persona[] personas, personasCopia1, personasCopia2, personasCopia3;
 		long tiempo;
-		int resul;
-		Persona persona = new Persona(Algoritmos.cedulaAleatorio(), Algoritmos.aleatorio(60, 100));
+		int resul = 0;						// cedula aleatoria			// entre 50kg y 100kg
+		Persona persona = new Persona(Algoritmos.cedulaAleatorio(), Algoritmos.aleatorio(50, 100));
 		
 		personas = generarPersonas(100);
 		System.out.println("Arreglo De 100 Personas");
@@ -22,21 +22,21 @@ public class Principal {
 		
 		System.out.println("Arreglo Ordenado Por BubbleSort");
 		tiempo = System.nanoTime();
-		Ordenacion.bubbleSort(personasCopia1);
+		Ordenacion.bubbleSort(personasCopia1); // personaCopia1 ordenada con bubbleSort
 		tiempo = System.nanoTime() - tiempo;
 		System.out.println("Tiempo Ejecucion: " + tiempo + " nanosegundos");
 		imprimir(personasCopia1);
 		
 		tiempo = System.nanoTime();
 		System.out.println("Arreglo Ordenado Por Seleccion");
-		Ordenacion.seleccion(personasCopia2);
+		Ordenacion.seleccion(personasCopia2); // personaCopia2 ordenada con seleccion
 		tiempo = System.nanoTime()-tiempo;
 		System.out.println("Tiempo Ejecucion: " + tiempo + " nanosegundos");
 		imprimir(personasCopia2);
 		
 		tiempo = System.nanoTime();
 		System.out.println("Arreglo Ordenado Por Insercion");
-		Ordenacion.insercion(personasCopia3);
+		Ordenacion.insercion(personasCopia3); // personaCopia3 ordenada con insercion
 		tiempo = System.nanoTime() - tiempo;
 		System.out.println("Tiempo Ejecucion: " + tiempo + " nanosegundos");
 		imprimir(personasCopia3);
@@ -45,32 +45,31 @@ public class Principal {
 		tiempo = System.nanoTime();
 		resul = Busqueda.busquedaSecuencial(persona, personas);
 		tiempo = System.nanoTime() - tiempo;
-      	System.out.println(personas + (resul == -1 ? "Dato No Encontrado" : "Dato Encontrado En La Posicion: " + resul));
+      	System.out.println(persona + (resul == -1 ? "\nDato No Encontrado" : "Dato Encontrado En La Posicion: " + resul));
     	System.out.println("Tiempo Ejecucion: " + tiempo + " nanosegundos\n");
       	
       	System.out.println("Busqueda Secuencial En Arreglo Ordenado");
 		tiempo = System.nanoTime();
 		resul = Busqueda.busquedaSecuencial(persona, personasCopia1);
 		tiempo = System.nanoTime() - tiempo;
-		System.out.println(personas + (resul == -1 ? "Dato No Encontrado" : "Dato Encontrado En La Posicion: " + resul));
+		System.out.println(persona + (resul == -1 ? "\nDato No Encontrado" : "Dato Encontrado En La Posicion: " + resul));
 		System.out.println("Tiempo Ejecucion: " + tiempo + " nanosegundos\n");
 		
 	 	System.out.println("Busqueda Binaria En Arreglo Ordenado");
 		tiempo = System.nanoTime();
 		resul = Busqueda.busquedaBinaria(persona, personasCopia2);
 		tiempo = System.nanoTime() - tiempo;
-		System.out.println(personas + (resul == -1 ? "Dato No Encontrado" : "Dato Encontrado En La Posicion: " + resul));
+		System.out.println(persona + (resul == -1 ? "\nDato No Encontrado" : "Dato Encontrado En La Posicion: " + resul));
 		System.out.println("Tiempo Ejecucion: " + tiempo + " nanosegundos\n");
 
 
-		
 	}
 	
 	public static Persona[] generarPersonas(int n) {
 		Persona[] vector = new Persona[n];
 		
 		for (int i = 0; i < vector.length; i++) {
-			vector[i] = new Persona(Algoritmos.cedulaAleatorio(), Algoritmos.aleatorio(60, 100));
+			vector[i] = new Persona(Algoritmos.cedulaAleatorio(), Algoritmos.aleatorio(50, 100)); // genera los datos para la cedula y el peso entre 50kg y 100kg
 		}
 		
 		return vector;
